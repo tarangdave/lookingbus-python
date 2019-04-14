24784acfff3f4fe095bae105aced1c0f53e9814a
Tarang Dave <tarangadave@gmail.com>
2019-04-14 13:27:11 -0700

    signup with firebase done

--- views.py
+++ views.py
@@ -2,11 +2,21 @@ import requests
 import sys
 import logging
 import json
+import pyrebase
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 globalStation = []
+
+config = {
+  "apiKey": "AIzaSyBcTW0gFNsGfU91lIb_7WCX1FPjLQyBnIA",
+  "authDomain": "lookingbus-237103.firebaseapp.com",
+  "databaseURL": "https://lookingbus-237103.firebaseio.com",
+  "storageBucket": "lookingbus-237103.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
 
 
 @app.route("/")
@@ -16,6 +26,22 @@ def home():
 @app.route("/map")
 def map():
     return render_template("map.html")
+
+@app.route("/signup", methods=["POST"])
+def signup():
+	username = request.form['username']
+	password = request.form['password']
+
+	db = firebase.database()
+	data = {
+		"username": username,
+		"password": password
+	}
+
+	db.child("users").push(data)
+
+	return json.dumps({"status": "success"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
